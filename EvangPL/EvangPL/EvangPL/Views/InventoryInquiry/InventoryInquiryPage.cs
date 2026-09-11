3ccb35828ccfd2c10f7b94e2dde7f2ab116a465d
chen <yang.chen@evangsol.co.jp>
2026-09-11 18:41:19 +0900

    test

--- EvangPL/EvangPL/EvangPL/Views/InventoryInquiry/InventoryInquiryPage.cs
+++ EvangPL/EvangPL/EvangPL/Views/InventoryInquiry/InventoryInquiryPage.cs
@@ -68,7 +68,7 @@ namespace EvangPL.Views.InventoryInquiry
 
         public InventoryInquiry() : base("strInventoryInquiry")
         {
-            Title = "在庫照会";
+            Title = "在庫照会1";
             BuildUI();
         }
 
