b99b805e580ad8719ff11372a199e03c82e44f8a
chen <yang.chen@evangsol.co.jp>
2026-09-14 11:53:22 +0900

    update Menu

--- EvangPL/EvangPL/EvangPL/Views/Menu/Menu.cs
+++ EvangPL/EvangPL/EvangPL/Views/Menu/Menu.cs
@@ -329,6 +329,7 @@ public class Menu : EvangContentVM
         LocalMemory.restlets.Add("GetItemrecept", "https://9323639-sb1.restlets.api.netsuite.com/app/site/hosting/restlet.nl?script=2063&deploy=1");
         LocalMemory.restlets.Add("Getitemfulfill", "https://9323639-sb1.restlets.api.netsuite.com/app/site/hosting/restlet.nl?script=2065&deploy=1");
         LocalMemory.restlets.Add("GetInventdetail", "https://9323639-sb1.restlets.api.netsuite.com/app/site/hosting/restlet.nl?script=2066&deploy=1");
+        LocalMemory.restlets.Add("GetAdjust", "https://9323639-sb1.restlets.api.netsuite.com/app/site/hosting/restlet.nl?script=2067&deploy=1");
         LocalMemory.restlets.Add("GetStockInDetail", "https://9323639-sb1.restlets.api.netsuite.com/app/site/hosting/restlet.nl?script=2061&deploy=1");
         LocalMemory.restlets.Add("SaveStockIn", "https://9323639-sb1.restlets.api.netsuite.com/app/site/hosting/restlet.nl?script=2061&deploy=1");
         LocalMemory.restlets.Add("GetOrderList", "https://9323639-sb1.restlets.api.netsuite.com/app/site/hosting/restlet.nl?script=2056&deploy=1");
