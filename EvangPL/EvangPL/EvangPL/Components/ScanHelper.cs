df06e91fb9e8650d2dec3afe32c8efbc0f3e2b1a
chen <yang.chen@evangsol.co.jp>
2026-09-23 18:23:08 +0900

    バーコードスキャン機能を追加

---
+++ EvangPL/EvangPL/EvangPL/Components/ScanHelper.cs
@@ -0,0 +1,19 @@
+﻿namespace EvangPL.Components
+{
+    public static class ScanHelper
+    {
+        /// <summary>
+        /// スキャンページを開き、結果をコールバックで返す
+        /// </summary>
+        /// <param name="navigation">呼び出し元の Navigation</param>
+        /// <param name="onResult">結果を受け取るコールバック</param>
+        public static async Task ScanAsync(INavigation navigation, Action<string> onResult)
+        {
+            if (navigation.NavigationStack.LastOrDefault() is ScanPage) return;
+
+            var scanPage = new ScanPage();
+            scanPage.BarcodeScanned += (s, code) => onResult(code);
+            await navigation.PushAsync(scanPage);
+        }
+    }
+}
